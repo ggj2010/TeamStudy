3a61dab6d0bfe90442f2e02ff3fdc8d9c352ef17
ggj2010 <gaoguangjin>
2015-05-08 09:30:39 +0800

    【a】

--- src/main/java/com/team/gaoguangjin/alibaba/dubbo/DemoServiceImpl.java
+++ src/main/java/com/team/gaoguangjin/alibaba/dubbo/DemoServiceImpl.java
@@ -1,9 +1,10 @@
 package com.team.gaoguangjin.alibaba.dubbo;
 
-
 public class DemoServiceImpl implements DemoService {
 	
 	public String sayHello(String name) {
+		
+		System.out.println("【返回值：】");
 		return "Hello Dubbo,Hello " + name;
 	}
 	
