5f0d89c4e27cb3f39e8e41a611d75308a6b8a060
ggj2010 <gaoguangjin>
2016-04-07 10:58:55 +0800

    test

---
+++ src/main/java/com/team/gaoguangjin/javabase/collection/list/TestListPerformance.java
@@ -0,0 +1,10 @@
+package com.team.gaoguangjin.javabase.collection.list;
+
+/**
+ * @author:gaoguangjin
+ * @Description:
+ * @Email:335424093@qq.com
+ * @Date 2016/4/7 10:40
+ */
+public class TestListPerformance {
+}
